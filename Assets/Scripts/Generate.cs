a13027c7b87b5c72b05b2323d64d88991f6e6f3e
Marco P <purichmarco@yahoo.ca>
2016-09-06 23:40:21 -0400

    Separate chunk generation and instantiation Chunks now generate dirt and grass on top of stone

--- Assets/Scripts/Generate.cs
+++ Assets/Scripts/Generate.cs
@@ -11,7 +11,12 @@ public class Generate : MonoBehaviour {
     private float zDistanceBlocks = 1.5f;
     #region Generation Information
     public GameObject source;
-    public GameObject hexObj;
+
+    //public GameObject hexObj;
+    public GameObject block_stone;
+    public GameObject block_dirt;
+    public GameObject block_grass;
+
     public AnimationCurve curve;
     public AnimationCurve curve2;
 
@@ -27,6 +32,8 @@ public class Generate : MonoBehaviour {
 
     private TrilinearInterpolation[] interpolators;
 
+    private enum BLOCKID : short {Air=0, Stone=1, Dirt=2, Grass=3 };
+
     public float pScale;
     public float pScale2;
 
@@ -37,8 +44,98 @@ public class Generate : MonoBehaviour {
 
     
 
-    public Chunk getChunk(Vector2 cPos, int size, int maxNumBlocks)
+    public short[,,] genChunk(Vector2 cPos, int size, int maxNumBlocks)
     {
+
+        short[,,] blockValues = new short[size, size, maxNumBlocks];
+
+        int chunkX = Mathf.FloorToInt(cPos.x);
+        int chunkZ = Mathf.FloorToInt(cPos.y);
+
+
+        float deltaX = (float)chunkX * (size * xDistanceBlocks);
+        float deltaZ = (float)chunkZ * (size * zDistanceBlocks);
+
+        float sum = 0;
+        foreach (float f in octaveWeights)
+        {
+            sum += f;
+        }
+
+        interpolators = new TrilinearInterpolation[octaveDistances.Length];
+
+        for (int d = 0; d < interpolators.Length; d++)
+        {
+            interpolators[d] = new TrilinearInterpolation(gameSeed * octaveSeeds[d], octaveDistances[d]);
+        }
+
+        //First pass for main stone generation
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                float blockX = i * 0.866f * 2.0f + (j % 2) * 0.866f + deltaX;
+                float blockZ = j * 1.5f + deltaZ;
+
+                for (int k = 0; k < maxNumBlocks; k++) {
+
+                    float chanceOfBlock = 0.0f;
+
+                    for (int oct = 0; oct < interpolators.Length; oct++)
+                    {
+                        chanceOfBlock += interpolators[oct].trilinearInterpolation(blockX, (float)k * blockSize, blockZ) * octaveWeights[oct];
+                    }
+
+                    chanceOfBlock = chanceOfBlock / (sum);
+
+                    float perlinValue = Mathf.PerlinNoise(blockX * pScale, blockZ * pScale);
+                    float perlinValue2 = Mathf.PerlinNoise((blockX + 456456) * pScale2, (blockZ + 12123) * pScale2); //random numbers to offset the noise so not same as perlinValue
+                    float heightFactor = ((float)k / maxNumBlocks) - (perlinValue2 * 0.25f); //Not sure what this 0.25f is, may or may not be min block height, should be looked into
+
+                    float combinedCurveValue = (perlinValue * curve.Evaluate(heightFactor)) + ((1.0f - perlinValue) * curve2.Evaluate(heightFactor)); //Mixing two curves based on noise
+
+                    if (chanceOfBlock < combinedCurveValue)
+                    {
+                        blockValues[i, j, k] = (short)BLOCKID.Stone;
+                    }
+                    else {
+                        blockValues[i, j, k] = (short)BLOCKID.Air;
+                    }
+                }
+            }
+        }
+
+        //Second pass for dirt/grass
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                short prev = (short)BLOCKID.Air;
+                int dirtCount = 0;
+
+                for (int k = maxNumBlocks - 1; k >= 0; k--) {
+                    //short id = blockValues[i, j, k];
+
+                    if (prev == (short)BLOCKID.Air && blockValues[i, j, k] != (short)BLOCKID.Air) {
+                        blockValues[i, j, k] = (short)BLOCKID.Grass;
+                    }
+                    
+                    if (prev == (short)BLOCKID.Grass || (prev == (short)BLOCKID.Dirt && dirtCount < 5))
+                    {
+                        blockValues[i, j, k] = (short)BLOCKID.Dirt;
+                        dirtCount++;
+                    }
+                    else {
+                        dirtCount = 0;
+                    }
+                    
+                    prev = blockValues[i, j, k];
+                }
+            }
+        }
+
+        return blockValues;
+
+        //Old getChunk function
+        /*
         GameObject holder = new GameObject("Holder of chunk of size " + size);
         if (chunks == null)
         {
@@ -72,6 +169,8 @@ public class Generate : MonoBehaviour {
         {
             interpolators[d] = new TrilinearInterpolation(gameSeed * octaveSeeds[d], octaveDistances[d]);
         }
+
+        
 
         for (int i = 0; i < size; i++)
         {
@@ -159,11 +258,93 @@ public class Generate : MonoBehaviour {
                 }
             }
         }
+
+    
         
 
         chunks.Add(result);
         temp.Add(holder);
         return result;
+
+        */
+    }
+
+    public Chunk instantiateChunk(Vector2 cPos, int size, int maxNumBlocks, short[,,] blockValues) {
+        GameObject holder = new GameObject("Holder of chunk of size " + size);
+        if (chunks == null)
+        {
+            chunks = new List<Chunk>();
+        }
+
+
+        int chunkX = Mathf.FloorToInt(cPos.x);
+        int chunkZ = Mathf.FloorToInt(cPos.y);
+
+
+        float deltaX = (float)chunkX * (size * xDistanceBlocks);
+        float deltaZ = (float)chunkZ * (size * zDistanceBlocks);
+
+        //Initialize our chunk
+        Chunk result = new Chunk();
+        result.hexObjs = new List<GameObject>();
+        result.pos = new Vector2(chunkX, chunkZ);
+        result.size = size;
+        //result.blockTypes = blockValues;
+
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                float blockX = i * 0.866f * 2.0f + (j % 2) * 0.866f + deltaX;
+
+                float blockZ = j * 1.5f + deltaZ;
+
+                short previousID = (short)BLOCKID.Air;
+                int blockStart = 0;
+
+                for (int k = 0; k < maxNumBlocks; k++) {
+                    short id = blockValues[i, j, k];
+
+                    if (id != previousID || k == maxNumBlocks)
+                    {
+                        //instantiate block if not air
+                        if (previousID != (short)BLOCKID.Air) {
+                            GameObject hObj = null;
+                            if(previousID == (short)BLOCKID.Stone)
+                                hObj = (GameObject)Instantiate(block_stone, new Vector3(0, 0, 0), block_stone.transform.rotation);
+                            else if(previousID == (short)BLOCKID.Dirt)
+                                hObj = (GameObject)Instantiate(block_dirt, new Vector3(0, 0, 0), block_dirt.transform.rotation);
+                            else if (previousID == (short)BLOCKID.Grass)
+                                hObj = (GameObject)Instantiate(block_grass, new Vector3(0, 0, 0), block_grass.transform.rotation);
+
+                            result.hexObjs.Add(hObj);
+                            hObj.transform.SetParent(holder.transform);
+
+                            hObj.transform.localScale = new Vector3(1, 1, (k-blockStart) * blockSize);
+                            hObj.transform.localPosition = new Vector3(blockX, blockStart * blockSize, blockZ);
+                            scaleUV(hObj);
+                        }
+
+                        blockStart = k;
+                        previousID = id;
+                    }
+                    else {
+                        //nothing to do
+                    }
+                }
+            }
+        }
+
+
+        chunks.Add(result);
+        temp.Add(holder);
+        return result;
+    }
+
+    //Generate and immediately instantiate a chunk
+    public Chunk genInstChunk(Vector2 cPos, int size, int maxNumBlocks) {
+        short[,,] blockValues = genChunk(cPos, size, maxNumBlocks);
+        return instantiateChunk(cPos, size, maxNumBlocks, blockValues);
     }
 
     //Scale the UV coordinates of a block's mesh
@@ -327,7 +508,7 @@ public class Generate : MonoBehaviour {
         loadedChunks = new Dictionary<Vector2, Chunk>();
         
 
-        Chunk firstChunk = getChunk(findCurrentChunk(source.transform.position), size, maxNumBlocks);
+        Chunk firstChunk = genInstChunk(findCurrentChunk(source.transform.position), size, maxNumBlocks);
         currentChunk.x = firstChunk.pos.x;
         currentChunk.y = firstChunk.pos.y;
 
@@ -375,7 +556,7 @@ public class Generate : MonoBehaviour {
             {
                 if (!loadedChunks.ContainsKey(new Vector2(startX + i, startZ + k))) {
                     //loadedChunks.Add((new Vector2(startX + i, startZ + k)), getChunk(new Vector3((startX + i) * xDistanceBlocks, 0, (startZ + k) * zDistanceBlocks), new Vector2(0, 0), size, maxNumBlocks));
-                    Chunk newC = getChunk(new Vector2(startX + i, startZ + k), size, maxNumBlocks);
+                    Chunk newC = genInstChunk(new Vector2(startX + i, startZ + k), size, maxNumBlocks);
                     loadedChunks.Add(new Vector2(startX + i, startZ + k), newC);
                     //Debug.Log((new Vector2(startX + i, startZ + k)));
                 }
